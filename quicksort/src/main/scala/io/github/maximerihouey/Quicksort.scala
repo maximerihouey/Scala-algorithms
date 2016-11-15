bdd33ca6e5eac297cb562cd0e19d0d6b54723d48
Maxime Rihouey <maxime.rihouey@gmail.com>
2016-11-16 00:13:52 +0100

    Introducing Quicksort

---
+++ quicksort/src/main/scala/io/github/maximerihouey/Quicksort.scala
@@ -0,0 +1,46 @@
+package io.github.maximerihouey
+
+/**
+  * Created by maxime on 28/10/16.
+  */
+object Quicksort {
+
+  def switch(array: Array[Int], index1: Int, index2: Int) = {
+    val temp = array(index1);
+    array(index1) = array(index2);
+    array(index2) = temp;
+  }
+
+  def partition(array: Array[Int], left: Int, right: Int, pivotIndex: Int): Int = {
+    val pivotValue: Int = array(pivotIndex);
+    var storeIndex: Int = left;
+    switch(array, pivotIndex, right);
+    for(i <- left to right) {
+      if(array(i) < pivotValue) {
+        switch(array, storeIndex, i);
+        storeIndex += 1;
+      }
+    }
+    switch(array, storeIndex, right);
+    return storeIndex;
+  }
+
+  def select(array: Array[Int], left: Int, right: Int, n: Int): Int = {
+    if(left == right) {
+      return array(left);
+    }
+    var pivotIndex = left + scala.util.Random.nextInt(right-left+1);
+    pivotIndex = partition(array, left, right, pivotIndex);
+    if(n == pivotIndex) {
+      return array(n);
+    } else if(n < pivotIndex) {
+      return select(array, left, pivotIndex-1, n);
+    }else{
+      return select(array, pivotIndex+1, right, n);
+    }
+  }
+
+  def quickselect(array: Array[Int], k: Int): Int = {
+    return select(array, 0, array.length-1, k-1);
+  }
+}
---
+++ quicksort/src/main/scala/io/github/maximerihouey/Test.scala
@@ -0,0 +1,60 @@
+package io.github.maximerihouey
+import scala.collection.mutable.ListBuffer;
+import java.io._
+
+/**
+  * Created by maxime on 29/10/16.
+  */
+object Test {
+
+  def range(n: Int, startIndex: Int, stepSize: Int): Array[Int] = {
+    val array = new Array[Int](n);
+    for (i <- 0 to (n - 1)) {
+      array(i) = startIndex + i * stepSize;
+    }
+    return array;
+  }
+
+  def fisherYatesShuffle(array: Array[Int]) = {
+    //    -- To shuffle an array a of n elements (indices 0..n-1):
+    //    for i from 0 to n−2 do
+    //      j ← random integer such that i ≤ j < n
+    //    exchange a[i] and a[j]
+    for(i <- 0 to array.length-2) {
+      val j = i + scala.util.Random.nextInt(array.length - i);
+      Quickselect.switch(array, i, j);
+    }
+  }
+
+  def timeExecution(example: Array[Int], k: Int, nbExampleBySize: Int): ListBuffer[Long] = {
+    var times = new ListBuffer[Long]();
+    for(i <- 1 to nbExampleBySize) {
+      fisherYatesShuffle(example)
+      val start = System.nanoTime();
+      Quickselect.quickselect(example, k);
+      times += System.nanoTime() - start;
+    }
+    return times;
+  }
+
+  def main(args: Array[String]) {
+
+    // FileWriter
+    val file = new File("benchmark_results.txt")
+    val bw = new BufferedWriter(new FileWriter(file))
+
+    //val exampleSizes = range(10, 100, 100);
+    val exampleSizes = Array[Int](50, 75, 100, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000, 7500, 9000, 10000, 15000, 17500, 20000, 25000);
+    val nbExampleBySize = 250;
+
+    for(exampleSize <- exampleSizes) {
+      println("%s".format(exampleSize))
+      val example = range(exampleSize, 1, 1);
+      val medianIndex = exampleSize/2;
+      val times = timeExecution(example, medianIndex, nbExampleBySize);
+      bw.write("%s %s\n".format(exampleSize, times.sum.toFloat / (times.length * scala.math.pow(10,9))));
+    }
+
+    bw.close()
+  }
+}
